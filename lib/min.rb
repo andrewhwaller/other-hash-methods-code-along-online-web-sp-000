f4c204b88a6e5bd214354aade7180d075f67ddb4
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 06:40:08 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -10,7 +10,7 @@ require 'pry'
 
 def get_the_min(groceries)
   grocery_values = []
-  groceries.each do { |food_group| grocery_values << food_group.values }
+  groceries.each do |food_group| grocery_values << food_group.values
   end
   grocery_values.sort_by { |h| h }
   grocery_values.first
